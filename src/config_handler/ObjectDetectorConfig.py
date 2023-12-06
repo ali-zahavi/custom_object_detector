c27d282fdc97ceefcd05318b150514cd4c588013
ali <ali.zahavi@gmail.com>
2023-12-06 15:23:34 +0000

    - ClassMapping dict converted to class_map.json - Add class_map.json to config.json - Modified ObjectDetectorConfig

--- src/config_handler/ObjectDetectorConfig.py
+++ src/config_handler/ObjectDetectorConfig.py
@@ -1,11 +1,15 @@
-from dataclasses import dataclass
-from dataclasses_json import dataclass_json
-from typing import List
+from dataclasses import dataclass, field
+from typing import List, Dict
+import json
 
-@dataclass_json
-@dataclass(frozen=True)
+@dataclass
 class ObjectDetectorConfig:
     model_path:str
     selected_classes: List[int]
     score_threshold:float
+    class_map_file: str
+    class_map: Dict[str, str] = field(init=False)
 
+    def __post_init__(self):
+        with open(self.class_map_file, 'r') as map_file:
+            self.class_map = json.load(map_file)
